0162884281cd4689a80b02b89f24d0ffaa3d0766
HYN8055 <hyn09102004@gmail.com>
2024-04-06 08:54:07 +0700

    First Commit 1

---
+++ src/main/emplyees/Main.java
@@ -0,0 +1,46 @@
+
+package main.emplyees;
+import view.employees.MainJFrame;
+//import doanjava.nhom6.qlchbqa.view.DangnhapJDialog;
+//import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author hyn09
+ */
+public class Main {
+    public static void main(String[] args) {
+    new MainJFrame().setVisible(true);
+    
+    
+    /*DangnhapJDialog dialog= new DangnhapJDialog(null,true);
+    dialog.setTitle("Đăng nhập hệ thống");
+    dialog.setResizable(false);
+    dialog.setLocationRelativeTo(null);
+    dialog.setVisible(true);
+   
+    dialog.getBtnDangNhap().addMouseListener(new MouseAdapter() {
+           @Override
+           public void mouseClicked(MouseEvent e) {
+                // Kiểm tra sự kiện nhấp chuột trái
+                  dialog.setVisible(false); // Ẩn dialog đăng nhập
+                  new MainJFrame().setVisible(true); // Hiển thị MainJFrame
+        
+          }
+    });*/
+    //MainJFrame mainFrame = new MainJFrame();
+    
+        
+        // Tạo dialog đăng nhập
+    /*DangnhapJDialog dialog = new DangnhapJDialog(null, true);
+    dialog.setTitle("Đăng nhập hệ thống");
+    dialog.setResizable(false);
+    dialog.setLocationRelativeTo(null);
+    dialog.setVisible(true);*/
+
+    // Thiết lập sự kiện lắng nghe cho nút đăng nhập
+    
+    }
+}
+
